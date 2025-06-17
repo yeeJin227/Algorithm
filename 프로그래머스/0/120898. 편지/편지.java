cf66628b607588a001bac602737e70163c626645
yeeJin227 <dpwls99600@gmail.com>
2025-06-17 18:43:08 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String message) {
+        int messageLength = message.length();
+        int answer = messageLength * 2;
+        
+        return answer;
+    }
+}
