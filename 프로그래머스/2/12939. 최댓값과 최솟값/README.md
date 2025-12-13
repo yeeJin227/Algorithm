ab29a55bb6a42975584104af6af1c347db07a1f7
yeeJin227 <dpwls99600@gmail.com>
2025-12-13 22:15:57 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 10.76 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/2/12939. 최댓값과 최솟값/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939) 
 
 ### 성능 요약
 
-메모리: 88.3 MB, 시간: 10.89 ms
+메모리: 92.5 MB, 시간: 10.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 02일 19:47:20
+2025년 12월 13일 22:15:54
 
 ### 문제 설명
 
--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,20 +1,24 @@
+import java.util.*;
+import java.io.*;
+
 class Solution {
     public String solution(String s) {
-        // 문자열을 공백으로 나누어 배열로 변환
-        String[] parts = s.split(" ");
         
-        // 첫 번째 값을 초기 최소/최대값으로 설정
-        int min = Integer.parseInt(parts[0]);
-        int max = Integer.parseInt(parts[0]);
+        String[] arr = s.split(" ");
         
-        // 나머지 숫자들을 확인하며 최소/최대 업데이트
-        for (String part : parts) {
-            int num = Integer.parseInt(part);
-            if (num < min) min = num;
-            if (num > max) max = num;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        
+        for(String str : arr){
+            int num = Integer.parseInt(str);
+            if(num > max){
+                max = num;
+            }
+            if(num < min){
+                min= num;
+            }
         }
         
-        // 결과 문자열 반환
-        return min + " " + max;
+        return min+" "+max;
     }
 }
